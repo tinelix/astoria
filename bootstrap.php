db4ed9fffb653cef028d333f6af09d90a3a6d0f1
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-04-22 20:52:00 +0700

    Redesign and rebranding but OpenVK base persisting

--- bootstrap.php
+++ bootstrap.php
@@ -244,7 +244,7 @@ return (function() {
     define('YEAR', 365 * DAY);
 
     define("nullptr", NULL);
-    define("OPENVK_DEFAULT_INSTANCE_NAME", "OpenVK", false);
+    define("OPENVK_DEFAULT_INSTANCE_NAME", "Astorium", false);
     define("OPENVK_VERSION", "Altair Preview ($ver)", false);
     define("OPENVK_DEFAULT_PER_PAGE", 10, false);
     define("__OPENVK_ERROR_CLOCK_IN_FUTURE", "Server clock error: FK1200-DTF", false);
