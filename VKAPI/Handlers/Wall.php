211fbca5ee02036f76336afb65665a1758bd5914
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-04-29 23:07:07 +0700

    VKAPI: Add "verified" prop in Wall.get or Newsfeed.get

--- VKAPI/Handlers/Wall.php
+++ VKAPI/Handlers/Wall.php
@@ -162,7 +162,8 @@ final class Wall extends VKAPIRequestHandler
                     "screen_name"       => $user->getShortCode(),
                     "photo_50"          => $user->getAvatarUrl(),
                     "photo_100"         => $user->getAvatarUrl(),
-                    "online"            => $user->isOnline()
+                    "online"            => $user->isOnline(),
+                    "verified"          => $user->isVerified()
                 ];
             }
 
@@ -177,6 +178,7 @@ final class Wall extends VKAPIRequestHandler
                     "photo_50"    => $group->getAvatarUrl(),
                     "photo_100"   => $group->getAvatarUrl(),
                     "photo_200"   => $group->getAvatarUrl(),
+                    "verified"    => $group->isVerified()
                 ];
             }
 
